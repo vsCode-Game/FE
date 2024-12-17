2d1c3a9029d05604d9859d79f91ec99dba3cf780
윤준수 <99115509+hoheesu@users.noreply.github.com>
2024-12-17 16:22:29 +0900

    fix: 배포 에러 수정

--- src/components/ui/AppButton.tsx
+++ src/components/ui/AppButton.tsx
@@ -2,12 +2,12 @@ import { ReactNode } from "react";
 import * as S from "./appButtonStyle";
 
 export default function AppButton(
-  // children: ReactNode,
+  children: ReactNode,
   { onClickFnc, type = "button", style = "default" }: IAppButtonProps,
 ) {
   return (
     <S.Button onClick={onClickFnc} type={type} $style={style}>
-      AppButton
+      {children}
     </S.Button>
   );
 }
