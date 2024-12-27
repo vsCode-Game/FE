496686d748b2f8f0a56cb1523ea89c6ffd579ace
danggin <danggin.min@gmail.com>
2024-12-27 16:37:10 +0900

    💄 style: Pretrendard Webfont 적용

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
 import GlobalStyles from "./styles/GlobalStyle";
+import "@styles/font.css";
 
 function App() {
   return (
