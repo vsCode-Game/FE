2b2679ca7576d519538f34d4318e29c9c1f07161
danggin <danggin.min@gmail.com>
2024-12-27 19:22:18 +0900

    🔧 conf: alias 적용 및 관련 모듈 타입 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import Header from "./components/layout/Header";
-import Footer from "./components/layout/Footer";
+import Header from "@components/layout/header/Header";
+import Footer from "@components/layout/footer/Footer";
 import GlobalStyles from "./styles/GlobalStyle";
 import "@styles/font.css";
 
