ac3d8c88ce2daa4ba85e02f2c42931d586360135
danggin <danggin.min@gmail.com>
2024-12-27 16:36:43 +0900

    🔧 conf: alias 설정

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,15 @@ export default defineConfig(({ mode }) => ({
   build: {
     outDir: mode === "production" ? "build" : "staging",
   },
+  resolve: {
+    alias: [
+      { find: "@", replacement: "/src" },
+      { find: "@assets", replacement: "/src/assets" },
+      { find: "@components", replacement: "/src/components" },
+      { find: "@pages", replacement: "/src/pages" },
+      { find: "@hooks", replacement: "/src/hooks" },
+      { find: "@styles", replacement: "/src/styles" },
+      { find: "@store", replacement: "/src/store" },
+    ],
+  },
 }));
