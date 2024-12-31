f59ebfc3aa49fe75a0eb8dfe0c9002fc67076784
salwwkd <hettzze@gmail.com>
2024-12-31 13:58:26 +0900

    ♻️ refactor: 유저 프로필 사진 및 닉네임 컴포넌트로 분리

---
+++ src/components/ui/nickname/NickNameBoxStyle.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const ProfileBox = styled.div`
+  display: flex;
+  line-height: 30px;
+  gap: 10px;
+`;
+export const Profile = styled.div`
+  width: 30px;
+  position: relative;
+  border: 2px solid #dedfe0;
+  border-radius: 50%;
+  max-width: 100%;
+  overflow: hidden;
+  height: 30px;
+  object-fit: cover;
+`;
