20a503ffa4dd61b13e576990a37e448e67c9f171
salwwkd <hettzze@gmail.com>
2024-12-31 14:00:00 +0900

    ✨ feat: 게임 입장 시 유저 프로필 상자 컴포넌트 추가

---
+++ src/components/ui/card/profileCard/ProfileCard.tsx
@@ -0,0 +1,10 @@
+import { CardIndex, PlayerCard } from "./ProfileCardStyle";
+
+export default function ProfileCard() {
+  return (
+    <>
+      <CardIndex></CardIndex>
+      <PlayerCard></PlayerCard>;
+    </>
+  );
+}
---
+++ src/components/ui/card/profileCard/ProfileCardStyle.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const CardIndex = styled.div`
+  width: 124px;
+  height: 46px;
+  flex-shrink: 0;
+  border-radius: 12px 12px 0px 0px;
+  background: #9cdcff;
+  box-shadow: 1px 1px 3px 0px rgba(0, 0, 0, 0.5);
+`;
+export const PlayerCard = styled.div`
+  width: 390px;
+  height: 220px;
+  flex-shrink: 0;
+  border-radius: 0px 12px 12px 12px;
+  background: #c8ecff;
+  box-shadow: 1px 1px 3px 0px rgba(0, 0, 0, 0.5);
+`;
