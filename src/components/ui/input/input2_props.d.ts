5d66b2076ade504a557786800a08ab2ea986d601
moonhuicheol <moonhuicheol@gmail.com>
2024-12-31 11:43:28 +0900

    ✨ feat: chatInputBox 구현

--- src/components/ui/input/input2_props.d.ts
+++
@@ -1,8 +0,0 @@
-import { HTMLInputTypeAttribute } from "react";
-import { Path } from "react-router-dom";
-
-interface IInputProps2<P> {
-  className?: string;
-  type: HTMLInputTypeAttribute;
-  keyname: Path<P>;
-}
