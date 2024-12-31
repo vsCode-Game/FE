17532f1c78913fb6882e2d8ce7ba2feaa983e1a9
moonhuicheol <moonhuicheol@gmail.com>
2024-12-31 11:01:25 +0900

    ✅ update: 로그인 title 태그변경

--- src/pages/user/login/LoginStyle.ts
+++ src/pages/user/login/LoginStyle.ts
@@ -22,7 +22,7 @@ export const label = styled.label`
   color: var(--color-gray-999);
 `;
 
-export const title = styled.div`
+export const title = styled.h2`
   font-weight: 700;
   font-size: 32px;
   color: var(--color-gray-999);
