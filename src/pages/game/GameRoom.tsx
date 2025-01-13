f93e7601a4279c9dffd1a1f4cb86ebe605fd8822
moonhuicheol <moonhuicheol@gmail.com>
2025-01-13 14:09:16 +0900

    ✨ feat: gameRoom 페이지 구현 및 레이아웃 설정

--- src/pages/game/GameRoom.tsx
+++ src/pages/game/GameRoom.tsx
@@ -1,3 +1,23 @@
+import ChattingBox from "../../components/ui/card/chatting/Chatting";
+import ProfileCard from "../../components/ui/card/profileCard/ProfileCard";
+import * as S from "./gameRoomStyle";
+
 export default function GameRoom() {
-  return <div>GameRoom</div>;
+  return (
+    <S.container>
+      <S.leftContainer>
+        <ProfileCard />
+        <ChattingBox />
+      </S.leftContainer>
+      <S.centerContainer>
+        <S.guideBox></S.guideBox>
+        <S.otherPlayerDeck></S.otherPlayerDeck>
+        <S.selectNumberBox></S.selectNumberBox>
+      </S.centerContainer>
+      <S.rightContainer>
+        <S.myDeckBox />
+        <ProfileCard />
+      </S.rightContainer>
+    </S.container>
+  );
 }
