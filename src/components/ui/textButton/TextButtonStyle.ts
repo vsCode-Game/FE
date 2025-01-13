d4c9efaa8a5c0e3a031f432330626cda0f9ab9cc
danggin <danggin.min@gmail.com>
2025-01-13 16:40:04 +0900

    ✨ feat: TextButton 컴포넌트 추가

---
+++ src/components/ui/textButton/TextButtonStyle.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledButton = styled.button`
+  text-decoration: underline;
+  font-size: 12px;
+  color: var(--color-gray-800);
+
+  &:hover {
+    color: var(--color-gray-999);
+  }
+`;
