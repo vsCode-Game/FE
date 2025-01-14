57959aee02ae179ce22d4a184930149d109b8aed
danggin <danggin.min@gmail.com>
2025-01-14 15:35:19 +0900

    ✏️ typos: 컨벤션에 맞게 interface 이름 수정

--- src/hooks/useFunnel.tsx
+++ src/hooks/useFunnel.tsx
@@ -1,22 +1,22 @@
 import { ReactElement, ReactNode, useState } from "react";
 
-export interface iStepProps {
+export interface IStepProps {
   name: string;
   children: ReactNode;
 }
 
-export interface iFunnelProps {
-  children: Array<ReactElement<iStepProps>>;
+export interface IFunnelProps {
+  children: Array<ReactElement<IStepProps>>;
 }
 
 export const useFunnel = (defaultStep: string) => {
   const [step, setStep] = useState(defaultStep);
 
-  const Step = (props: iStepProps): ReactElement => {
+  const Step = (props: IStepProps): ReactElement => {
     return <>{props.children}</>;
   };
 
-  const Funnel = ({ children }: iFunnelProps) => {
+  const Funnel = ({ children }: IFunnelProps) => {
     const targetStep = children.find(
       (childStep) => childStep.props.name === step,
     );
