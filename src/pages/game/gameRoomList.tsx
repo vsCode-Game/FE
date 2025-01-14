4970611d58130e43c27974cfc5fd8b900f726a8b
moonhuicheol <moonhuicheol@gmail.com>
2025-01-14 09:39:26 +0900

    ✨ feat: gameRoomList axios 및 reactQuery적용

--- src/pages/game/gameRoomList.tsx
+++ src/pages/game/gameRoomList.tsx
@@ -1,85 +1,43 @@
-import { useInfiniteQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import Button from "../../components/ui/button/Button";
 import * as S from "./gameRoomListStyle";
 import GameRoomCard from "../../components/ui/card/cardList/GameRoomCard";
-import { useCallback, useEffect, useRef } from "react";
+import axios, { AxiosError } from "axios";
 
-const mockData = Array.from({ length: 100 }, (_, index) => ({
-  id: index + 1,
-  roomName: `테스트방${index + 1}`,
-  maxPlayers: 2,
-  currentCount: (index % 2) + 1,
-  createdAt: new Date().toISOString(),
-}));
+export const instance = axios.create({
+  baseURL: "https://api.davincicodegame.store",
+});
 
-// const fetchRooms = async ({ pageParam = 1 }) => {
-//   const res = await fetch(`/gameRoom?page=${pageParam}&limit=12`);
-//   if (!res.ok) {
-//     throw new Error("failed to fetch game room");
-//   }
-//   const data = await res.json();
-//   return {
-//     rooms: data,
-//     nextPage: data.length === 10 ? pageParam + 1 : undefined,
-//   };
-// };
+interface IGameRoom {
+  id: number;
+  roomName: string;
+  maxPlayers: number;
+  currentCount: number;
+  createdAt: string;
+}
 
-const fetchMockRooms = async ({ pageParam = 1, limit = 12 }) => {
-  const start = (pageParam - 1) * limit;
-  const end = start + limit;
-  const pageData = mockData.slice(start, end);
+export const getGameRoomsList = async () => {
+  try {
+    const { data } = await instance.get("/gameRoom");
+    return data;
+  } catch (error) {
+    const axiosError = error as AxiosError<unknown>;
+    if (axiosError.response) {
+      return axiosError.response.data;
+    }
+  }
+};
 
-  return {
-    rooms: pageData,
-    nextPage: end < mockData.length ? pageParam + 1 : undefined,
-  };
+export const useGetGameRooms = () => {
+  return useQuery({
+    queryKey: ["gameRoomsList"],
+    queryFn: getGameRoomsList,
+  });
 };
 
 export default function GameRoomList() {
-  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } =
-    useInfiniteQuery(
-      ["mockGameRooms"], // Query Key
-      ({ pageParam = 1 }) => fetchMockRooms({ pageParam, limit: 12 }),
-      {
-        getNextPageParam: (lastPage) => lastPage.nextPage, // 다음 페이지 결정
-      },
-    );
-  // Intersection Observer를 위한 ref
-  const loadMoreRef = useRef(null);
-
-  // Intersection Observer 콜백
-  const handleObserver = useCallback(
-    (entries: any) => {
-      const target = entries[0];
-      if (target.isIntersecting && hasNextPage) {
-        fetchNextPage(); // 다음 페이지 로드
-      }
-    },
-    [fetchNextPage, hasNextPage],
-  );
-
-  // Observer 등록
-  useEffect(() => {
-    const observer = new IntersectionObserver(handleObserver, {
-      root: null, // 뷰포트를 기준으로
-      rootMargin: "0px",
-      threshold: 1.0, // 요소가 100% 보일 때 트리거
-    });
-
-    if (loadMoreRef.current) {
-      observer.observe(loadMoreRef.current);
-    }
-
-    return () => {
-      if (loadMoreRef.current) {
-        observer.unobserve(loadMoreRef.current);
-      }
-    };
-  }, [handleObserver]);
-  if (status === "loading") return <p>Loading...</p>;
-
-  // 모든 페이지 데이터를 평탄화
-  const allRooms = data?.pages.flatMap((page) => page.rooms);
+  const { data: gameRooms } = useGetGameRooms();
+  console.log("gameRooms✅", gameRooms);
 
   return (
     <S.container>
@@ -95,15 +53,10 @@ export default function GameRoomList() {
         </Button>
       </S.buttonContainer>
       <S.gameroomListContainer>
-        {allRooms?.map((room) => (
+        {gameRooms?.map((room: IGameRoom) => (
           <GameRoomCard key={room.id} room={room} />
         ))}
       </S.gameroomListContainer>
-      <div
-        ref={loadMoreRef}
-        style={{ height: "1px", background: "transparent" }}
-      />
-      {isFetchingNextPage && <p>Loading more...</p>}
     </S.container>
   );
 }
