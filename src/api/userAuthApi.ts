ff1da63bcd872394f3ff9b22a2dcc41241195f5f
moonhuicheol <moonhuicheol@gmail.com>
2025-01-14 14:32:46 +0900

    ✨ feat: 로그인 기능 구현

--- src/api/userAuthApi.ts
+++ src/api/userAuthApi.ts
@@ -1,14 +1,56 @@
-import { instance } from "./axios";
+import { authInstance, instance } from "./axios";
+import { AxiosError } from "axios";
+
+export interface ILoginResponse {
+  accessToken: string;
+}
+
+export interface IErrorResponse {
+  status: number;
+  message: string;
+}
 
 export const loginUser = async (logInfo: {
   email: string;
   password: string;
-}) => {
-  const response = instance.post("/auth/login", logInfo, {
-    withCredentials: true, // 쿠키를 포함시키기 위해 withCredentials 옵션을 true로 설정합니다.
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-  console.log(response);
+}): Promise<ILoginResponse> => {
+  try {
+    console.log(instance, "인스턴스확인");
+    const response = await instance.post<ILoginResponse>(
+      "/auth/login",
+      logInfo,
+      {
+        withCredentials: true,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+    );
+
+    console.log(response);
+    return response.data;
+  } catch (error) {
+    const axiosError = error as AxiosError<IErrorResponse>;
+    if (axiosError.response) {
+      console.log(axiosError.response.data.status);
+      throw new Error(axiosError.response.data.message); // 에러 메시지 던지기
+    } else {
+      throw new Error("로그인 중 예상치 못한 오류가 발생했습니다.");
+    }
+  }
+};
+
+export const refreshToken = async () => {
+  try {
+    const response = await authInstance.post("/auth/refresh", {
+      withCredentials: true,
+    });
+    console.log(response);
+    const accessToken = response?.headers.authorization;
+
+    localStorage.setItem("accessToken", accessToken);
+    return response.data.accessToken;
+  } catch (err) {
+    console.log(err);
+  }
 };
