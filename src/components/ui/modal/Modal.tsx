3c57062c7eac67cbac8666212d80c8382c588746
dain <danggin.min@gmail.com>
2025-01-17 04:49:21 +0900

    ✨ feat: 모달 실행 시 스크롤 방지 기능 추가

--- src/components/ui/modal/Modal.tsx
+++ src/components/ui/modal/Modal.tsx
@@ -24,6 +24,19 @@ export default function Modal() {
   if (!showModal) return null;
 
   useEffect(() => {
+    document.body.style.cssText = `
+      position: fixed; 
+      top: -${window.scrollY}px;
+      overflow-y: scroll;
+      width: 100%;`;
+    return () => {
+      const scrollY = document.body.style.top;
+      document.body.style.cssText = "";
+      window.scrollTo(0, parseInt(scrollY || "0", 10) * -1);
+    };
+  }, []);
+
+  useEffect(() => {
     const onKeydownEscape = (e: KeyboardEvent) => {
       if (e.key === "Escape") {
         closeModal();
