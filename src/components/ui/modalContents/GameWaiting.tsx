6ac8fb7bd4c54397984be0f25cd6776f6a66520d
dain <danggin.min@gmail.com>
2025-01-17 03:02:42 +0900

    ✨ feat: 게임 시작 기다리기 모달에 로딩 컴포넌트 추가

--- src/components/ui/modalContents/GameWaiting.tsx
+++ src/components/ui/modalContents/GameWaiting.tsx
@@ -1,10 +1,11 @@
+import Loading from "../loading/Loading";
 import { Container, Contents, ContentsContainer } from "./gameWaitingStyle";
 
 export default function GameWaiting() {
   return (
     <>
       <Container>
-        {/* 로딩 컴포넌트 */}
+        <Loading overlay={false} />
         <ContentsContainer>
           <Contents>잠시 후 게임이 시작됩니다.</Contents>
           <Contents>조금만 기다려 주세요. 😊</Contents>
