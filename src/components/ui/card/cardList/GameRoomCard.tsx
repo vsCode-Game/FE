d3874ee093e170f07e0cfd6fb6bbc1e8f982e760
moonhuicheol <moonhuicheol@gmail.com>
2025-01-17 13:56:11 +0900

    ✨ feat:  badge 상태 구현

--- src/components/ui/card/cardList/GameRoomCard.tsx
+++ src/components/ui/card/cardList/GameRoomCard.tsx
@@ -1,21 +1,27 @@
+import { IGameRoom } from "../../../../api/gameRoomApi";
 import { useJoinGameRoomMutation } from "../../../../hooks/useMutation";
 import { useRoomIdStore } from "../../../../store/useRoomIdStore";
 import Badge from "../../badge/Badge";
 import Button from "../../button/Button";
 import * as S from "./GameRoomCardStyle";
 
-export default function GameRoomCard({ room }: { room: IRoomProps }) {
-  const { setRoomId } = useRoomIdStore();
+export default function GameRoomCard({ room }: { room: IGameRoom }) {
   const disabled = room.currentCount === 2 ? true : false;
   const mutation = useJoinGameRoomMutation();
   const onClickJoin = (roomId: number) => {
-    setRoomId(roomId);
     mutation.mutate(roomId);
   };
 
+  const roomState = () => {
+    if (room.currentCount === 1) {
+      return "waiting...";
+    } else if (room.currentCount === 2) {
+      return "playing";
+    }
+  };
   return (
     <S.cardContainer>
-      <Badge playerNumber={room.currentCount} />
+      <Badge playerNumber={room.currentCount} status={roomState()} />
       <S.titleBox>{room.roomName}</S.titleBox>
       <S.buttonContainer>
         <Button
