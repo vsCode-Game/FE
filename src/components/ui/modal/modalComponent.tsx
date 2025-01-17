45db651469bf550fdd9b57e29262c909341f5880
moonhuicheol <moonhuicheol@gmail.com>
2025-01-17 15:38:18 +0900

    🐛 fix: 모달수정

--- src/components/ui/modal/modalComponent.tsx
+++ src/components/ui/modal/modalComponent.tsx
@@ -4,6 +4,7 @@ import BigDeck from "../bigDeck/BigDeck";
 import TurnWaiting from "../modalContents/TurnWaiting";
 import GameWaiting from "../modalContents/GameWaiting";
 import Alert from "../modalContents/Alert";
+import GuessNumber from "../selectionNumber/GuessNumber";
 
 export const modalComponents: Record<string, React.ReactNode> = {
   firstSelect: <FirstSelect />,
@@ -11,7 +12,10 @@ export const modalComponents: Record<string, React.ReactNode> = {
   selectWhiteBlack: <BigDeck />,
   turnWaiting: <TurnWaiting />,
   gameWaiting: <GameWaiting />,
-  GamrRoomJoinConfirm: (
+  guessNumber: <GuessNumber />,
+  myTurn: <BigDeck />,
+
+  GameRoomJoinConfirm: (
     <Alert message="개임 방 만들기는 로그인 후 가능합니다." />
   ),
 };
