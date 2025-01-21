d1c02d2a190620278acc3a0c1a5cb0082d835810
salwwkd <hettzze@gmail.com>
2025-01-21 11:17:55 +0900

    ✨ feat: 랭킹페이지 구현

--- src/components/ui/ranking/rankingListStyle.ts
+++ src/components/ui/ranking/rankingListStyle.ts
@@ -1,5 +1,43 @@
 import styled from "styled-components";
 
-export const GoldImage = styled.img``;
-export const SilverImage = styled.img``;
-export const BronzeImage = styled.img``;
+export const RankingBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0px 44px 0px 61px;
+`;
+export const NickNameBox = styled.div`
+  display: flex;
+  gap: 20px;
+`;
+export const GoldImage = styled.img`
+  width: 30px;
+  height: 30px;
+`;
+export const SilverImage = styled.img`
+  width: 30px;
+  height: 30px;
+`;
+export const BronzeImage = styled.img`
+  width: 30px;
+  height: 30px;
+`;
+export const NicknameImage = styled.img`
+  width: 40px;
+  height: 40px;
+`;
+export const NickNameText = styled.div`
+  color: var(--gray-999);
+  font-size: 18px;
+  font-weight: 500;
+`;
+export const RankingNumber = styled.div`
+  color: var(--gray-999);
+  font-size: 24px;
+  font-weight: 600;
+`;
+export const Hr = styled.hr`
+  width: 400px;
+  height: 1px;
+  background: var(--gray-999);
+`;
