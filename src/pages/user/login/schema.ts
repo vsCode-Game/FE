55bdabe577cbe05d3dafa08b7fd2376bbe60e189
moonhuicheol <moonhuicheol@gmail.com>
2025-02-28 17:28:36 +0900

    로그인 zod 수정

--- src/pages/user/login/schema.ts
+++ src/pages/user/login/schema.ts
@@ -5,7 +5,10 @@ export const schema: z.ZodType<loginSchema> = z.object({
   userEmail: z.string().email("이메일 형식이 아닙니다."),
   password: z
     .string()
-    .min(8, { message: "비밀번호는 최소 8자리 이상 입력해주세요." })
+    .min(8, {
+      message:
+        "비밀번호는 숫자와 특수문자를 포함하여 8자리 이상 입력해 주세요.",
+    })
     .regex(regexPassword, {
       message:
         "비밀번호는 숫자와 특수문자를 포함하여 8자리 이상 입력해 주세요.",
